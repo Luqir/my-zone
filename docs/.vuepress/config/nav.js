742a514b2be304dc0dbf97accf0193f96bb17bb8
luqirong <luqir123@qq.com>
2021-01-13 14:46:51 +0800

    先关闭 考试模块 和 网工软考

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,11 +1,11 @@
 // 导航栏配置
 module.exports = [
   { text: 'Home', link: '/', icon: 'reco-home' },
-  {
-    text: 'Docs',
-    icon: 'reco-document',
-    items: [{ text: '网工软考', link: '/views/docs/network-engineer/' }]
-  },
+  // {
+  //   text: 'Docs',
+  //   icon: 'reco-document',
+  //   items: [{ text: '网工软考', link: '/views/docs/network-engineer/' }]
+  // },
   { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
   { text: 'Exam', link: '/views/others/exam.md', icon: 'reco-other' }
   // {
--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,7 +1,7 @@
 // 插件配置
 module.exports = [
   // 考试/练习模块插件
-  require('../plugins/exam/'),
+  // require('../plugins/exam/'),
   require('../plugins/svg-icon/'),
   // 更新刷新插件
   [
